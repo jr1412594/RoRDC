b4b450632f70dd93222da86a859956106d6727c9
jr1412594 <jr.medina1412@gmail.com>
2021-07-07 15:38:05 -0600

    hw for gets

---
+++ The_Ruby_Programming_Language/analyzer.rb
@@ -0,0 +1,18 @@
+=begin
+    enter your first name
+    enter you last name
+    puts your full neme
+    puts your name reversed
+    puts length of your name 
+=end
+
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+
+full_name = "#{first_name} #{last_name}"
+
+puts "Your full name is #{full_name}"
+puts "Your full name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.length} characters in it"
