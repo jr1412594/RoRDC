ae6c5b8876b55a844c0edae1804e6627c935ac5c
jr1412594 <jr.medina1412@gmail.com>
2021-07-07 15:17:45 -0600

    first commit

---
+++ The_Ruby_Programming_Language/playground.rb
@@ -0,0 +1,13 @@
+# puts "Hello World!"
+
+# greeting = "Hello Jr!"
+
+# puts greeting
+# p greeting
+# print greeting
+
+def say_hello(name)
+    puts "Hello #{name}"
+end
+
+p say_hello('Karina')
---
+++ The_Ruby_Programming_Language/strings_part1.rb
@@ -0,0 +1,14 @@
+# sentance = "My name is Jr"
+# p sentance
+
+first_name = "jr"
+last_name = "Medina"
+
+full_name = "#{first_name} #{last_name}"
+
+# p first_name
+# p last_name
+# p full_name
+
+p full_name.class
+
